bcb3f8e095ee92021d300f1620414e6c4fadf330
Alexander <kolo-ky@yandex.ru>
2021-02-12 13:50:27 +0500

    added css-modules webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -18,16 +17,23 @@ module.exports = {
               test: /\.(js|jsx)$/,
               exclude: /node_modules/,
               use: {
-              loader: 'babel-loader',
+                loader: 'babel-loader',
               },
           },
           {
             test: /\.css$/,
             use: [
               'style-loader',
-              'css-loader'
-            ]
-          }
+              {
+                loader: 'css-loader',
+                options: {
+                  modules: {
+                    localIdentName: "[name]__[local]___[hash:base64:5]",
+                  },
+                }
+              },
+            ],
+          },
         ],
     },
     resolve: {
