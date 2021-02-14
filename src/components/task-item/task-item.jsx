6bde513febdfcef49496d328c189d3961112ad40
Alexander <kolo-ky@yandex.ru>
2021-02-14 19:00:04 +0500

    task border color by board

--- src/components/task-item/task-item.jsx
+++ src/components/task-item/task-item.jsx
@@ -4,18 +4,23 @@ import React, {useState} from 'react';
 import classnames from "classnames";
 import style from "../../assets/common-styles/comon.style.css";
 
+const TASK_COLOR_BY_BOARD_ID = {
+  0: 'taskBacklog',
+  1: 'taskProcessing',
+  2: 'taskDone',
+  3: 'taskBasket',
+}
+
 const TaskItem = ({task}) => {
   const [isTaskEdit, toggleEditTask] = useState();
   const [taskTitle, setTaskTitle] = useState(task.title);
 
   const handleSetTaskTitle = ({target}) => {
     setTaskTitle(target.value);
-    console.log(taskTitle);
   }
 
   const handleToggleTask = () => {
     if (isTaskEdit) {
-      console.log('emit task');
       toggleEditTask(false);
     } else {
       toggleEditTask(true);
@@ -23,7 +28,12 @@ const TaskItem = ({task}) => {
   };
 
   return (
-    <div className={classnames(style.taskBoardItem, style.task, isTaskEdit ? style.taskActive : null)}>
+    <div className={classnames(
+      style.taskBoardItem,
+      style.task,
+      style[TASK_COLOR_BY_BOARD_ID[task.boardId]],
+      isTaskEdit ? style.taskActive : null
+    )}>
       <div className={classnames(style.taskBody)}>
         {
           isTaskEdit
