a8fff50d3c56ebd4ae1dc46f2ba177d581e24c1d
Alexander <kolo-ky@yandex.ru>
2021-02-14 14:40:19 +0500

    hide clear button when tasks is empty

--- src/components/boards/trash/trash.jsx
+++ src/components/boards/trash/trash.jsx
@@ -16,16 +16,21 @@ const Trash = ({tasks, title}) => {
       <TaskList tasks={tasks}>
         <TrashEmpty />
       </TaskList>
-
-      <button className={classnames(style.taskBoardButton, style.button, style.buttonClear)} type="button">
-        <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <rect x="15.5374" y="5.16638" width="1.83333" height="14.6667" transform="rotate(45 15.5374 5.16638)"
-                fill="white"/>
-          <rect x="16.8337" y="15.5372" width="1.83333" height="14.6667" transform="rotate(135 16.8337 15.5372)"
-                fill="white"/>
-        </svg>
-        <span>Очистить</span>
-      </button>
+      {
+        tasks.length
+        ?
+            <button className={classnames(style.taskBoardButton, style.button, style.buttonClear)} type="button">
+              <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect x="15.5374" y="5.16638" width="1.83333" height="14.6667" transform="rotate(45 15.5374 5.16638)"
+                      fill="white"/>
+                <rect x="16.8337" y="15.5372" width="1.83333" height="14.6667" transform="rotate(135 16.8337 15.5372)"
+                      fill="white"/>
+              </svg>
+              <span>Очистить</span>
+            </button>
+          :
+            null
+      }
     </article>
   );
 };
