246377a8146191b2cf7cd46ef53cee06eda3f454
Alexander <kolo-ky@yandex.ru>
2021-02-21 16:25:44 +0500

    refactor(header)

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import PropTypes from 'prop-types';
 
 // styles
 import style from './header.module.css';
 
-const Header = ({children}) => {
+// components
+import User from "../user/user";
+
+const Header = () => {
   return (
     <header className={style.boardHeader}>
       <div className={style.boardInner}>
         <h1><Link to="/">WorkUp</Link></h1>
-        {children}
+        <User />
       </div>
     </header>
   );
-};
-
-Header.propTypes = {
-  children: PropTypes.object
 };
 
 export default Header;
--- src/components/user/user.jsx
+++ src/components/user/user.jsx
@@ -1,43 +1,32 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import PropTypes from 'prop-types';
 
 // styles
 import style from './user.module.css';
 import classnames from 'classnames';
 
-const ALLOWED_SCREENS = [`Main`];
-
-const User = ({screen}) => {
+const User = () => {
   return (
     <ul className={classnames(style.user)}>
       <li className={classnames(style.userAvatar)}>
         A
       </li>
-      {
-        ALLOWED_SCREENS.includes(screen) && (
-          <li className={classnames(style.userMenu)}>
-            <ul className={classnames(style.menu)}>
-              <li className={classnames(style.menuItem)}>
-                <Link to="/profile">
-                  Профиль
-                </Link>
-              </li>
-              <li className={classnames(style.menuItem)}>
-                <Link to="/">
-                  Выйти
-                </Link>
-              </li>
-            </ul>
+      <li className={classnames(style.userMenu)}>
+        <ul className={classnames(style.menu)}>
+          <li className={classnames(style.menuItem)}>
+            <Link to="/profile">
+              Профиль
+            </Link>
           </li>
-        )
-      }
+          <li className={classnames(style.menuItem)}>
+            <Link to="/">
+              Выйти
+            </Link>
+          </li>
+        </ul>
+      </li>
     </ul>
   );
-};
-
-User.propTypes = {
-  screen: PropTypes.string.isRequired
 };
 
 export default User;
