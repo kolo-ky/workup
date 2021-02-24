cda9bbb479d390b289f84d298d5f6ebffa7ffba3
Alexander <kolo-ky@yandex.ru>
2021-02-25 01:55:29 +0500

    rename cn to classnames

--- src/components/screens/login-screen/login.tsx
+++ src/components/screens/login-screen/login.tsx
@@ -9,26 +9,26 @@ import {ServiceLayout} from "../../layouts/service-layout";
 
 // styles
 import style from './login.module.css';
-import cn from 'classnames';
+import classnames from 'classnames';
 
 const Login: FC = () => {
   return (
     <ServiceLayout>
-      <section className={cn(style.loginSection)}>
-        <h1 className={cn(style.loginTitle)}>WorkUp</h1>
-        <form action="#" className={cn(style.loginForm)} aria-label="Форма авторизации">
+      <section className={classnames(style.loginSection)}>
+        <h1 className={classnames(style.loginTitle)}>WorkUp</h1>
+        <form action="#" className={classnames(style.loginForm)} aria-label="Форма авторизации">
           <label htmlFor="login">Логин</label>
           <input type="text" name="login" id="login" placeholder="Ваш логин..." required />
 
           <label htmlFor="password">Пароль</label>
           <input type="password" name="password" id="password" placeholder="Ваш пароль..." required />
 
-          <button className={cn(style.loginButton)} type="submit">
+          <button className={classnames(style.loginButton)} type="submit">
             <span>Войти</span>
           </button>
         </form>
         <p>
-          <Link className={cn(style.authLink)} to={routes('auth')}>В первый раз? Зарегистрироваться</Link>
+          <Link className={classnames(style.authLink)} to={routes('auth')}>В первый раз? Зарегистрироваться</Link>
         </p>
       </section>
     </ServiceLayout>
